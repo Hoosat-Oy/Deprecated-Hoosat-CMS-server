05e607819ac019cf4ea82df80e5d113de5d851d2
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-04-21 14:48:30 +0300

    Meh

--- src/server.ts
+++ src/server.ts
@@ -57,7 +57,7 @@ app.use(cors({ origin : async (origin: any, callback: (arg0: Error | null, arg1:
 }, credentials: true }));
 
 
-import { EnableRoutes } from "./routes/";
+import { EnableRoutes } from "./routes";
 EnableRoutes(app);
 
 import { loginDiscord } from "./lib/discord/discord";
